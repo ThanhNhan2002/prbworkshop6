f8b28f9f9e3c5bb420f1302762cbb692db4150d6
ThanhNhanNguyen <thanhnhan.b12002@gmail.com>
2022-08-15 10:56:02 +1000

    fix error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-  return res.send('Hello World2');
+  return res.send('Hello World');
 });
 
 app.listen(process.env.PORT||8000, ()=> {
